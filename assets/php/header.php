0036e35b58cf4c03a0d3fb632e4b8540ad440aa2
Ethan Henderson <ethan@zbee.codes>
2021-07-15 04:35:43 -0600

    base

--- assets/php/header.php
+++ assets/php/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">" rel="stylesheet">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
         <meta name="description" content="A FFXIV tool to get market prices from universalis.app and tell you the seal-purchasable item that is being actively sold for the most gil" />
         <meta name="keywords" content="ffxiv, ffxiv market, ffxiv universalis, grand company, company seals, grand company seals, gil, how to make gil, ffxiv gil" />
