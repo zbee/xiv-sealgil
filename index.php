1490fe81c0cb8b955716ef32b535bc4e04090af4
Ethan Henderson <ethan@zbee.codes>
2021-07-15 07:16:55 -0600

    search results styling

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ $("#worldSearch").keyup(function(e) {
 
       searchResults.append("<div class='worldSearchResult flex mx-auto px-5 hover:bg-gray-700 py-2 px-5 cursor-pointer' data-world='" + result.item.world
       + "'><div class='w-1/3 text-gray-300'>" + result.item.world
-      + "</div><div class='w-2/3 text-gray-400'>(" + result.item.group + " in " + result.item.region + ")</div></div>");
+      + "</div><div class='w-2/3 text-gray-400 text-right'>(" + result.item.group + " in " + result.item.region + ")</div></div>");
 
       x++;
     }
