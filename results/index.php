b62135601404b9f4208ec50a1b5af0b6f6decd26
Ethan Henderson <ethan@zbee.codes>
2021-07-15 11:33:45 -0600

    debug search results

--- results/index.php
+++ results/index.php
@@ -2,6 +2,8 @@
 require('../assets/php/header.php');
 require('../assets/php/exchangeItems.php');
 
+date_default_timezone_set("UTC");
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
