e1d1c497a8a9e198e67ea8d3054baf30abc62c76
Ethan Henderson <ethan@zbee.codes>
2021-07-15 04:48:53 -0600

    colors

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require('./assets/php/header.php'); ?>
 
-<div class="min-h-screen bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
+<div class="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div class="absolute inset-0 bg-gradient-to-r from-red-600 to-yellow-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-      <div class="relative px-4 py-10 bg-gray-400 shadow-lg sm:rounded-3xl sm:p-20">
+      <div class="absolute inset-0 bg-gradient-to-r from-yellow-100 to-yellow-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div class="relative px-4 py-10 bg-gray-700 shadow-lg sm:rounded-3xl sm:p-20">
       <div class="max-w-md mx-auto">
           <div>
           <img src="/assets/img/sealgil.png" class="h-32 mx-auto" />
