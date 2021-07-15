23fe117b586f69a9861f79174c05d8c66d0214de
Ethan Henderson <ethan@zbee.codes>
2021-07-15 05:07:52 -0600

    base

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div class="absolute inset-0 bg-gradient-to-r from-yellow-100 to-yellow-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div class="absolute inset-0 bg-gradient-to-r from-yellow-600 to-yellow-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
       <div class="relative px-4 py-10 bg-gray-700 shadow-lg sm:rounded-3xl sm:p-20">
       <div class="max-w-md mx-auto">
           <div>
@@ -18,7 +18,7 @@
           <ul class="list-disc space-y-2">
             <li class="flex items-start">
               <span class="h-6 flex items-center sm:h-7">
-                <svg class="flex-shrink-0 h-5 w-5 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
+                <svg class="flex-shrink-0 h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
               </span>
@@ -28,7 +28,7 @@
             </li>
             <li class="flex items-start">
               <span class="h-6 flex items-center sm:h-7">
-                <svg class="flex-shrink-0 h-5 w-5 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
+                <svg class="flex-shrink-0 h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
               </span>
@@ -38,7 +38,7 @@
             </li>
             <li class="flex items-start">
               <span class="h-6 flex items-center sm:h-7">
-                <svg class="flex-shrink-0 h-5 w-5 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
+                <svg class="flex-shrink-0 h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
               </span>
