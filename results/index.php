cf7eb399779a749fe0cbf5d14815396de5925ebe
Ethan Henderson <ethan@zbee.codes>
2021-07-21 15:29:33 -0600

    adjust efficiency threshold

--- results/index.php
+++ results/index.php
@@ -40,8 +40,8 @@ $countEfficiencyWithinHighThreshold = 0;
 ///////////////////////////////////////////////////////////////////////////
 
 //Pruning/Rating thresholds (on a -2 to +2 scale)
-$thresholdEfficiencyGood = 0.5;
-$thresholdEfficiencyHigh = 1;
+$thresholdEfficiencyGood = 0.25;
+$thresholdEfficiencyHigh = 0.5;
 
 $thresholdSaleVelocityOne = 5;
 $thresholdSaleVelocityTwo = 10;
