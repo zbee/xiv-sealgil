90be2cae2d0f2f79952617e5d4b30ff2828545f8
Ethan Henderson <ethan@zbee.codes>
2021-07-21 13:07:56 -0600

    increase width

--- assets/php/header.php
+++ assets/php/header.php
@@ -27,7 +27,7 @@
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
       <div class="absolute inset-0 bg-gradient-to-r from-yellow-600 to-yellow-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
       <div class="relative px-4 py-10 bg-gray-700 shadow-lg sm:rounded-3xl sm:p-20">
-      <div class="max-w-md mx-auto">
+      <div class="max-w-lg mx-auto">
 
         <div>
             <a href="https://xiv-sealgil.herokuapp.com/"><img src="/assets/img/sealgil.png" class="h-32 mx-auto" /></a>
