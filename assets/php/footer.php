c959e4d90d6673afcd39861b683ac8d82c32a9fe
Ethan Henderson <ethan@zbee.codes>
2022-03-02 10:20:12 -0700

    bump version

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -2,7 +2,7 @@
                 </div>
                 
                 <div class="pt-6 text-xs text-gray-500">
-                    Built by <a href="https://keybase.io/zbee" class="underline">Ethan</a> in 2021 (updated to patch <b>5.5</b>),
+                    Built by <a href="https://keybase.io/zbee" class="underline">Ethan</a> in 2021 (updated to patch <b>6.08</b>),
                     Utilizing data from <a href="https://universalis.app" class="underline">Universalis</a>,
                     Using resources from <a href="https://www.finalfantasyxiv.com" class="underline">FFXIV</a>.
                     Running on <a href="https://heroku.com" class="underline">Heroku</a> with
